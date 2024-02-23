1dc35430e4e378416b6bf7ce33082703e6f46aa7
Rubyy007 <batchamanic960@gmail.com>
2024-02-23 12:27:29 +0530

    login register coding

---
+++ src/donarselect.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function donarselect() {
+  return (
+    <div>donarselect</div>
+  )
+}
+
+export default donarselect
--- src/donerlog.jsx
+++ src/donerlog.jsx
@@ -1,27 +1,74 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 function Donerlog() {
+    const hp = useNavigate()
+    const [formData, setFormData] = useState({
+        Email: "",
+        password: "",
+    })
+
+    const handleClick = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log("Email:", formData.Email);
+        console.log("password:", formData.password);
+try{
+    const res = await axios(
+        {
+            url: 'http://localhost:7007/user/donarlog',
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            }, body: JSON.stringify(formData),
+            data: formData
+        })
+
+    if (res.status >= 200 && res.status < 300) {
+        alert("login success")
+        hp("/")
+    } else if (res.status >= 400){
+        alert("User Not Found or Wrong Password ")
+    }else{
+        alert("login error")
+    }
+
+
+
+}catch{err => {
+    console.log("err in log req",err);
+}}
+       
+    }
+
     return (
 
         <div className='donerreg'>
-            <Form>
-            <h1>Doner Login</h1>
+            <Form onSubmit={handleSubmit}>
+            <h1>Donar Login</h1>
             <Form.Group className="mb-3" >
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="Email" name='Email' value={formData.Email} onChange={handleClick} placeholder="Enter Email" />
 
             </Form.Group>
 
 
             <Form.Group className="mb-3" >
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name='password'  value={formData.password} onChange={handleClick} type="password" placeholder="Password" />
             </Form.Group>
 
 
-            <Button style={{ width: "100%" }} variant="primary" type="submit">
+            <Button onClick={handleSubmit} style={{ width: "100%" }} variant="primary" type="submit">
                 Login
             </Button>
 
