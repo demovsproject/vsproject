47712d0a6f152ff48e08ebeb0f5afa8c6b141089
demovsproject <demo.vsproject@gmail.com>
2025-01-14 23:45:54 -0800

    Update Aboutus.jsx

--- src/Aboutus.jsx
+++ src/Aboutus.jsx
@@ -13,7 +13,8 @@ function Aboutus() {
         <div className="content">
           <h1><span>About Us</span><br /></h1>
           <p>
-            The MVV NGO is the Non-Government Organization which provide blood donation and organ donation direct to the customers and doners with the aggrecation of NGO. The NGO is Organized by the Founders Manikavasakar, Vengadeshwaran and Vignesh. Which was focus to deliver Organs without any interrupts or extra fee.
+single para content
+Welcome to Vistawin Solutions! At Vistawin Solutions, we are dedicated to providing cutting-edge software solutions tailored to your business needs. Whether you're looking to streamline operations with our ERP systems, enhance customer relationships with our CRM software, or optimize your supply chain, our expert team is here to deliver innovative, reliable, and efficient solutions. With a focus on quality and customer satisfaction, we strive to empower businesses to achieve their full potential and drive success. Let's elevate your business together!
           </p>
         </div>
         <div className="image">
