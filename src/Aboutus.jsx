706ae453a72ad7d1dec10f5422c1c6c5c283d1d4
demovsproject <demo.vsproject@gmail.com>
2025-01-14 23:48:07 -0800

    Update Aboutus.jsx

--- src/Aboutus.jsx
+++ src/Aboutus.jsx
@@ -13,7 +13,6 @@ function Aboutus() {
         <div className="content">
           <h1><span>About Us</span><br /></h1>
           <p>
-single para content
 Welcome to Vistawin Solutions! At Vistawin Solutions, we are dedicated to providing cutting-edge software solutions tailored to your business needs. Whether you're looking to streamline operations with our ERP systems, enhance customer relationships with our CRM software, or optimize your supply chain, our expert team is here to deliver innovative, reliable, and efficient solutions. With a focus on quality and customer satisfaction, we strive to empower businesses to achieve their full potential and drive success. Let's elevate your business together!
           </p>
         </div>
@@ -24,7 +23,7 @@ Welcome to Vistawin Solutions! At Vistawin Solutions, we are dedicated to provid
       </header>
     </div>
     <footer>
-      <p>&copy; 2024 MVV NGO. All rights reserved.</p>
+      <p>&copy; 2024 vistawin solution. All rights reserved.</p>
     </footer>
   </>
 </div>
